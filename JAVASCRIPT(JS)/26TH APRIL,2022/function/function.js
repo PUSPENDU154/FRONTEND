eec1bd1569224e211ab1523182fb89a954c2ae0f
PUSPENDU154 <puspendusaha154@gmail.com>
2022-05-06 13:57:53 +0530

    learnings of 26th april with index of js

--- JAVASCRIPT(JS)/26TH APRIL,2022/function/function.js
+++ JAVASCRIPT(JS)/26TH APRIL,2022/function/function.js
@@ -5,7 +5,7 @@ function multiply(p1,p2){
 function divide(p1,p2){
     return (p1/p2)
 }
-console.log(divide(56,8))
+// console.log(divide(56,8))
 
 function toCelcius(fahrenheit){
     return (5/9)*(fahrenheit-32)
@@ -13,3 +13,18 @@ function toCelcius(fahrenheit){
 
 let x=toCelcius(99)
 document.write("The Temperature is "+ x +" degree Celcius")//(2:01:00)
+
+let object2={
+    firstName:"Puspendu",
+    lastName:"Saha",
+    age:21,
+    fullName:function(){
+        return this.firstName+" "+this.lastName
+    },
+}
+
+console.log(object2)
+console.log(object2.fullName())// call the name fetched by "this" keyword
+console.log(object2.fullName) // call the whole function inside the object
+console.log(object2["firstName"])
+
